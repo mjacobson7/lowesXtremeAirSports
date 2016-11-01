1aac6a31e1a85a8227061a17e6f81dea984b0e40
maxjacobson <max.jacobson@torchlms.com>
2016-11-01 13:39:48 -0600

    update code

--- public/features/activities/activitiesCtrl.js
+++ public/features/activities/activitiesCtrl.js
@@ -1,12 +1,5 @@
 angular.module('myApp')
   .controller('activitiesCtrl', function($scope, $state, mainService) {
 
-  //   $(document).ready(function(){
-  //   $('.slider').slider({
-	// 		full_width: true,
-	// 		height: 600,
-	// 		transition: 200
-	// 	});
-  // });
 
   }); //end
