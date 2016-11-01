b1ba5dc9ab007c29f2d0a0de4452a79fcd0a109e
maxjacobson <max.jacobson@torchlms.com>
2016-10-31 23:08:11 -0600

    update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 var app = require('./config/express.js')();
 require('./config/database.js')();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
 
 app.listen(port, function() {
   console.log('The magic happens on port ' + port);
 });
+
+//server file
